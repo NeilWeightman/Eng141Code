3936e803283ab5823b3ad6c349a045cac3318f8b
Neil Weightman <82580266+NeilWeightman@users.noreply.github.com>
2023-01-03 14:55:39 +0000

    start controller

---
+++ SakilaJPA/src/main/java/com/sparta/sakilajpa/controllers/SakilaApiController.java
@@ -0,0 +1,108 @@
+package com.sparta.sakilajpa.controllers;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.sparta.sakilajpa.entities.Actor;
+import com.sparta.sakilajpa.repositories.ActorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class SakilaApiController {
+    @Autowired
+    private ActorRepository actorRepo;
+
+    // method mapped onto /sakila/hello
+    @GetMapping("/sakila/hello")
+    public String sayHello(){
+        return "<h1>Hello, world!</h1>";
+    }
+
+    @GetMapping("/sakila/newActor")
+    public Actor returnActor(){
+        Actor actor = new Actor();
+        actor.setFirstName("Yash");
+        actor.setLastName("Jagani");
+        actor.setId(1234);
+        return actor;
+    }
+
+    @GetMapping("/sakila/newActors")
+    public List<Actor> returnActors(){
+        Actor actor = new Actor();
+        actor.setFirstName("Yash");
+        actor.setLastName("Jagani");
+        actor.setId(1234);
+        Actor actor2 = new Actor();
+        actor2.setFirstName("Hanibal");
+        actor2.setLastName("Brhanu");
+        actor2.setId(4321);
+        List<Actor> result = new ArrayList<>();
+        result.add(actor);
+        result.add(actor2);
+        return result;
+    }
+
+    @GetMapping("sakila/actors/testRead")
+    public Actor findActor15(){
+        Optional<Actor> result = actorRepo.findById(15);
+        Actor cuba = result.get();
+        return cuba;
+    }
+
+//    @GetMapping("sakila/actors/{id}")
+//    public Actor findActor(@PathVariable int id){
+//        Optional<Actor> result = actorRepo.findById(id);
+//        Actor actor = result.get();
+//        return actor;
+//    }
+
+    @GetMapping("sakila/actors/{id}")
+    public ResponseEntity<String> findById(@PathVariable int id){
+        Optional<Actor> result = actorRepo.findById(id);
+        ObjectMapper actorMapper = JsonMapper.builder()
+                .addModule(new JavaTimeModule())
+                .build();
+        ResponseEntity<String> returnValue = null;
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("content-type", "application/json");
+        if(result.isPresent()){
+            Actor actor = result.get();
+            String actorString = null;
+            try {
+                actorString = actorMapper.writeValueAsString(actor);
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            }
+            returnValue = new ResponseEntity<>(actorString, headers, HttpStatus.OK);
+        } else {
+            returnValue = new ResponseEntity<>(
+                    "{\"message\":\"Actor "+id+" not found\"}",
+                    headers,
+                    HttpStatus.NOT_FOUND);
+        }
+        return returnValue;
+    }
+
+    @GetMapping("sakila/actors")
+    public Actor findActorParam(@RequestParam int id, @RequestParam String firstName){
+        System.out.println(firstName);
+        Optional<Actor> result = actorRepo.findById(id);
+        Actor actor = result.get();
+        return actor;
+    }
+    @ExceptionHandler
+    public String reportError(Throwable t){
+    System.out.println(t);
+        return "<h1>Error! You messed up.</h1>";
+    }
+}
